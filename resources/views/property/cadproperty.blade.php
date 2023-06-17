26ebd4377cc7359d8373e8762d2ddabf67decb75
Bouamra Fathi <fbouamra@gmail;COM>
2023-06-17 14:37:48 +0100

    Mergeremote-trackingbranch 'origin/main' into main

--- resources/views/property/cadproperty.blade.php
+++ resources/views/property/cadproperty.blade.php
@@ -1,10 +1,12 @@
 <div class="card  mb-3" style="max-width: 18rem;">
     <div class="card-header ">
 
-        <a class="nav-link " href="#"> {{$property->title}}</a>
+        <a class="nav-link " href="{{route('property.show',$property)}}"> {{$property->title}}</a>
     </div>
     <div class="card-body">
         <h5 class="card-title">{{$property->adress}}</h5>
+
+        <img class="card-img-top" src="/storage/{{ $property->photo }}" alt="Property Photo">
         <p class="card-text">{{$property->descreption}}</p>
         <li><small>Nombre de pieces : {{$property->rooms}}</small></li>
         <li><small>Nombre de pieces a coucher : {{$property->bedrooms}}</small></li>
